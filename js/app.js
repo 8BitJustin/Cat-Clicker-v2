0d6d8120a419ace3e8d85f6fd2505983b91b574c
Justin Olson <j.olson.digital@gmail.com>
2018-08-22 21:53:14 -0700

    HTML layout in place, minus text within images

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
-const imageOne = document.getElementById('kitteh-img-one');
-const imageTwo = document.getElementById('kitteh-img-two');
-const images = document.querySelectorAll('.kitteh-img');
-let firstCounter = document.getElementById('firstCounter');
-let secondCounter = document.getElementById('secondCounter');
-let totalCounter = document.getElementById('totalCounter');
+// const imageOne = document.getElementById('kitteh-img-one');
+// const imageTwo = document.getElementById('kitteh-img-two');
+// const images = document.querySelectorAll('.kitteh-img');
+// let firstCounter = document.getElementById('firstCounter');
+// let secondCounter = document.getElementById('secondCounter');
+// let totalCounter = document.getElementById('totalCounter');
 
-// Creates names for kittehs. Simple names could have been given, but what fun is that?
-const kittehOneName = document.getElementById('kitteh-one-name');
-const kittehTwoName = document.getElementById('kitteh-two-name');
-const names = ['Tigger', 'Tiger', 'Max', 'Sirena', 'Sammie', 'Smokey', 'Kitty', 'Sassy', 'Shadow', 'Simba', 'Patch', 'Lucky', 'Misty', 'Princess', 'Boots', 'Oliver', 'Lucy', 'Precious', 'Oscar', 'Fluffy', 'Whiskers'];
-function randomNames(names) {
-    return names[Math.floor(Math.random()*names.length)];
-};
-kittehOneName.innerHTML = randomNames(names);
-kittehTwoName.innerHTML = randomNames(names);
-if (kittehOneName.innerHTML === kittehTwoName.innerHTML) {
-    console.log('Match, rerolling kittehTwoName');
-    kittehTwoName.innerHTML = randomNames(names);
-};
+// // Creates names for kittehs. Simple names could have been given, but what fun is that?
+// const kittehOneName = document.getElementById('kitteh-one-name');
+// const kittehTwoName = document.getElementById('kitteh-two-name');
+// const names = ['Tigger', 'Tiger', 'Max', 'Sirena', 'Sammie', 'Smokey', 'Kitty', 'Sassy', 'Shadow', 'Simba', 'Patch', 'Lucky', 'Misty', 'Princess', 'Boots', 'Oliver', 'Lucy', 'Precious', 'Oscar', 'Fluffy', 'Whiskers'];
+// function randomNames(names) {
+//     return names[Math.floor(Math.random()*names.length)];
+// };
+// kittehOneName.innerHTML = randomNames(names);
+// kittehTwoName.innerHTML = randomNames(names);
+// if (kittehOneName.innerHTML === kittehTwoName.innerHTML) {
+//     console.log('Match, rerolling kittehTwoName');
+//     kittehTwoName.innerHTML = randomNames(names);
+// };
 
-// Adds event listner to each image. On click, console says which is clicked and the counter increments by 1
-imageOne.addEventListener('click', function() {
-    console.log('one clicked!');
-    firstCounter.innerText++;
-});
-imageTwo.addEventListener('click', function() {
-    console.log('two clicked!');
-    secondCounter.innerText++;
-});
-for (let i = 0; i < images.length; i++) {
-    // Adds event listner to both images. On click, console says 'total clicked', and the total counter increments by 1
-    images[i].addEventListener('click', function() {
-        console.log('total clicked!');
-        totalCounter.innerText++;
-    });
-};
+// // Adds event listner to each image. On click, console says which is clicked and the counter increments by 1
+// imageOne.addEventListener('click', function() {
+//     console.log('one clicked!');
+//     firstCounter.innerText++;
+// });
+// imageTwo.addEventListener('click', function() {
+//     console.log('two clicked!');
+//     secondCounter.innerText++;
+// });
+// for (let i = 0; i < images.length; i++) {
+//     // Adds event listner to both images. On click, console says 'total clicked', and the total counter increments by 1
+//     images[i].addEventListener('click', function() {
+//         console.log('total clicked!');
+//         totalCounter.innerText++;
+//     });
+// };
