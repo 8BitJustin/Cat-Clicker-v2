94988c0fbc23e7a400356ec37956cd84622a8c30
Justin Olson <j.olson.digital@gmail.com>
2018-08-24 07:10:12 -0700

    Final version. Finished switch, works properly with displayImg id. Also created initial starting text that disappears on first cat selection

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const introText = document.getElementById('intro');
 const displayImg = document.getElementById('img-display-id');
 const imgSmall = document.querySelectorAll('.img-small');
 let imgSmallOne = document.getElementById('img-list-count-id-1');
@@ -10,8 +11,10 @@ let listCount = 0;
 
 for(let i = 0; i < imgSmall.length; i++) {
     imgSmall[i].addEventListener('click', function() {
+        introText.style.display = 'none';
         console.log('clicked');
         displayImg.src = imgSmall[i].src;
+        displayImg.id = imgSmall[i].id;
         listCount = imgSmall[i].previousElementSibling.innerText;
         document.getElementById('img-display-count').innerText = listCount;
         document.getElementById('img-display-name').innerText = imgSmall[i].parentNode.firstElementChild.innerText;
@@ -20,82 +23,28 @@ for(let i = 0; i < imgSmall.length; i++) {
 
 displayImg.addEventListener('click', function() {
     displayCount.innerText++;
-    switch() {
-        case 'http://127.0.0.1:5500/img/kitteh.jpeg':
+    switch(displayImg.id) {
+        case 'img-one':
+            console.log('one');
             imgSmallOne.innerText++;
             break
-        case 'img/kittehtwo.jpg':
+        case 'img-two':
+            console.log('two');
             imgSmallTwo.innerText++;
             break
-        case 'img/kittehthree.jpg':
+        case 'img-three':
+            console.log('three');
             imgSmallThree.innerText++;
             break
-        case 'img/kittehfour.jpg':
+        case 'img-four':
+            console.log('test four');
             imgSmallFour.innerText++;
             break
-        case 'img/kittehfive.jpg':
+        case 'img-five':
+            console.log('test five');
             imgSmallFive.innerText++;
             break
         default:
             console.log('default');
       };
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * Script for first version, kept as reference
- */
-
-// const imageOne = document.getElementById('kitteh-img-one');
-// const imageTwo = document.getElementById('kitteh-img-two');
-// const images = document.querySelectorAll('.kitteh-img');
-// let firstCounter = document.getElementById('firstCounter');
-// let secondCounter = document.getElementById('secondCounter');
-// let totalCounter = document.getElementById('totalCounter');
-
-// // Creates names for kittehs. Simple names could have been given, but what fun is that?
-// const kittehOneName = document.getElementById('kitteh-one-name');
-// const kittehTwoName = document.getElementById('kitteh-two-name');
-// const names = ['Tigger', 'Tiger', 'Max', 'Sirena', 'Sammie', 'Smokey', 'Kitty', 'Sassy', 'Shadow', 'Simba', 'Patch', 'Lucky', 'Misty', 'Princess', 'Boots', 'Oliver', 'Lucy', 'Precious', 'Oscar', 'Fluffy', 'Whiskers'];
-// function randomNames(names) {
-//     return names[Math.floor(Math.random()*names.length)];
-// };
-// kittehOneName.innerHTML = randomNames(names);
-// kittehTwoName.innerHTML = randomNames(names);
-// if (kittehOneName.innerHTML === kittehTwoName.innerHTML) {
-//     console.log('Match, rerolling kittehTwoName');
-//     kittehTwoName.innerHTML = randomNames(names);
-// };
-
-// // Adds event listner to each image. On click, console says which is clicked and the counter increments by 1
-// imageOne.addEventListener('click', function() {
-//     console.log('one clicked!');
-//     firstCounter.innerText++;
-// });
-// imageTwo.addEventListener('click', function() {
-//     console.log('two clicked!');
-//     secondCounter.innerText++;
-// });
-// for (let i = 0; i < images.length; i++) {
-//     // Adds event listner to both images. On click, console says 'total clicked', and the total counter increments by 1
-//     images[i].addEventListener('click', function() {
-//         console.log('total clicked!');
-//         totalCounter.innerText++;
-//     });
-// };
